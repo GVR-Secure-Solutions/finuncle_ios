1d384c557a2da4fd4c584dabf6be43b5fe72d89a
Rahul <rk1821389@gmail.com>
2023-03-31 09:32:44 +0530

    Initial Commit

---
+++ Finuncle/FinuncleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinuncleApp.swift
+//  Finuncle
+//
+//  Created by Rahul on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FinuncleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
